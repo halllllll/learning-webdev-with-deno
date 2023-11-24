1126c3de6542ccc96d94b25bc6c78dd6368b25cd
h4lllllll <hjod1172@yahoo.co.jp>
2023-11-24 11:22:13 +0000

    import modules and add credential info

--- server.deno.js
+++ server.deno.js
@@ -1,7 +1,13 @@
 import { serveDir } from 'https://deno.land/std@0.208.0/http/file_server.ts';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.38.5';
+import 'https://deno.land/std@0.208.0/dotenv/load.ts';
+
+const SUPABASE_URL = Deno.env.get('SUPABASE_URL');
+const SUPABASE_API_KEY = Deno.env.get("SUPABASE_API_KEY");
+
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
 Deno.serve((req) => {
-
     return serveDir(req, {
         fsRoot: 'public',
         urlRoot: '',
